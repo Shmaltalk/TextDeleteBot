13a3eb06b4ad15e988e515c62311b55d09a0fe87
jkairoy <jkairoy@gmail.com>
2020-05-11 15:07:33 -0700

    timing push

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.login(process.env.TOKEN)
 function setAlert() {
   // Timeout at 10 every day
   var now = new Date();
-  var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 45) - now;
+  var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 7, 0) - now;
   if (millisTill10 < 0) {
        millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
   }
@@ -42,7 +42,7 @@ client.on("message", msg => {
 client.on('guildMemberAdd', member => {
   stupidanimals = ["Chicken", "Iguana", "Kangaroo", "Anchovy", "Dingo",
                    "Dolphin", "Emu", "Penguin", "Zebra", "Gopher", "Aardvark",
-                   "Llama", "Beaver", "Gnu"]
+                   "Llama", "Gnu"]
   stupidcolors = ["Ginger", "Lavendar", "Sage", "Turquoise", "Violet",
                   "Vermilion", "Tangerine", "Apricot", "Indigo", "Canary",
                   "Aquamarine", "Cobalt", "Magenta", "Sepia", "Copper",
@@ -50,7 +50,14 @@ client.on('guildMemberAdd', member => {
   combo = stupidcolors[Math.floor(Math.random() * stupidanimals.length)] + " " +
           stupidanimals[Math.floor(Math.random() * stupidanimals.length)]
   member.setNickname(combo);
-  member.send(`Welcome to chime!\n- 1st step, block dms\n- 2nd step, turn off
-              all notifications, make sure @channel and @here are still active\n
-              this how it work mon, post once a day, youll get a message from admins`)
+  member.send(`Welcome to chime!
+**Heres how to get set up:**
+- 1st step: block direct messaging by clicking on the "Chime" server name in the top left corner (make sure you are on the chime server page), going to "Privacy Settings", and disabling direct messaging
+- 2nd step: set your notification settings by going to "Notification Settings" in the server settings tab, (again accessed by clicking on the "Chime" server name while on the chime server). From there, check "Only @mentions"\n\n
+**Now that your setup is done, you can start posting!**
+Chime only allows for emoji based text posts, so any message you send containing text will be deleted.
+We prefer that you post in a format of one main emoji, representing your mood, followed by an arbitrary number of "story emojis", which will be used to describe your mood. Try to separate your main emoji from your story emojis with a dash. Example: 😊-🎨📚📖☀️
+We encourage members to post once a day and we will be sending daily reminders to help you remember to post!
+We encourage you to also use the discord reaction feature to respond to other posts.
+Thanks so much for helping us out and have fun using Chime!`)
 })
